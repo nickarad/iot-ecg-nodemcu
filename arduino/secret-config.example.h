ac1645cd086028ac5af8bfa94d69cf58c43e4221
karadalex <karadalex@gmail.com>
2019-07-14 22:52:47 +0300

    arduino secret-config

---
+++ arduino/secret-config.example.h
@@ -0,0 +1,6 @@
+// Update these with values suitable for your network.
+const char* ssid = "your_ssid";
+const char* password = "your_password";
+const char* mqtt_server = "192.168.0.121"; 
+const char* mqtt_user = "admin";
+const char* mqtt_pass= "admin";
---
+++ docs/arduino/quickstart.md
@@ -0,0 +1,3 @@
+# Quickstart
+
+Copy 'secret-config.example.h' to 'secret-config.h' and update the values
