b7127cd8c7b72f73e3bc0eadc06be781c3a77a0f
nickarad <nickarad94@gmail.com>
2019-07-15 13:32:33 +0300

    Real Time Plot Data

--- sub/test_sub.py
+++ sub/test_sub.py
@@ -1,6 +1,23 @@
 # Python MQTT subscriber test
 #
 import paho.mqtt.client as mqtt
+import matplotlib.pyplot as plt #import matplotlib library
+from drawnow import *
+
+plt.ion() #Tell matplotlib you want interactive mode to plot live data
+cnt=0
+tempF= []
+
+def makeFig(): #Create a function that makes our desired plot
+    plt.ylim(150,350)                                 #Set y min and max values
+    plt.title('My Live Streaming Sensor Data')      #Plot the title
+    plt.grid(True)                                  #Turn the grid on
+    plt.ylabel('Temp F')                            #Set ylabels
+    plt.plot(tempF, 'ro-', label='Degrees F')       #plot the temperature
+    plt.legend(loc='upper left')                    #plot the legend
+                  #plot the legend
+
+
  
 def on_connect(client, userdata, flags, rc):
     print("Connected to broker")
@@ -8,6 +25,14 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, message):
     print("Received message '" + str(message.payload) + "' on topic '"
         + message.topic + "' with QoS " + str(message.qos))
+    temp =  float(message.payload)
+    tempF.append(temp) 
+    drawnow(makeFig)                       #Call drawnow to update our live graph
+    plt.pause(.000001)                     #Pause Briefly. Important to keep drawnow from crashing
+    cnt=cnt+1
+    if(cnt>50):                            #If you have 50 or more points, delete the first one from the array
+        tempF.pop(0)                       #This allows us to just see the last 50 data points
+        
 
 client = mqtt.Client()
 client.username_pw_set("rabbitmq", password='rabbitmq')
