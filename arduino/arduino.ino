21dde2a12be68a7e70d746c15066b73f0f6e1440
nickarad <nickarad94@gmail.com>
2019-07-18 15:26:01 +0300

    Fix Indents

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -21,79 +21,59 @@ float getECG(void) {
 }
 
 void setup_wifi() {
-  // Connecting to a WiFi network
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-	 delay(500);
-	 Serial.print(".");
-  }
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+	// Connecting to a WiFi network
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
-// float getTemperature() {
-//   const int tempPin = 17;
-//   int reading = analogRead(tempPin);  
- 
-// 	// converting that reading to voltage, for 3.3v arduino use 3.3
-//   float voltage = reading * 3.3;
-//   voltage /= 1024.0; 
-//   float temperatureC = (voltage - 0.5) * 100 ;
-//   Serial.print(temperatureC);
-//   Serial.println(" °C");
-//   return temperatureC;
-// }
-
-// String getTemperatureJSON() {
-//   return String("{\"temperature\":\"")+String(getTemperature())+String("\"}");
-// }
-
 void reconnect() {
-  // Loop until we're reconnected
-  Serial.println("In reconnect...");
-  while (!client.connected()) {
-	 Serial.print("Attempting MQTT connection...");
-	 // Attempt to connect
-	 if (client.connect("Arduino_Gas", mqtt_user, mqtt_pass)) {
-		Serial.println("connected");
-	 } else {
-		Serial.print("failed, rc=");
-		Serial.print(client.state());
-		Serial.println(" try again in 5 seconds");
-		delay(5000);
-	 }
-  }
+	// Loop until we're reconnected
+	Serial.println("In reconnect...");
+	while (!client.connected()) {
+		Serial.print("Attempting MQTT connection...");
+		// Attempt to connect
+		if (client.connect("Arduino_Gas", mqtt_user, mqtt_pass)) {
+			Serial.println("connected");
+		} 
+		else {
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			Serial.println(" try again in 5 seconds");
+			delay(5000);
+		}
+	}
 }
 
 void setup() {
-  Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
+	Serial.begin(115200);
+	setup_wifi();
+	client.setServer(mqtt_server, 1883);
 }
 
 void loop() {
-  // msg = getTemperatureJSON();
-	// msg.toCharArray(payload, 50);
+  
+	char msg[8];
 
-  char msg[8];
+	// Desired sample rate T=7812microseconds
+	if (micros() - lastMicros > 7812) {
+		lastMicros = micros();
+		// float temperatureC = getTemperature() ;
+		float ecg = getECG();
+		sprintf(msg,"%f",ecg);
+		// Serial.print("temperature read");
+		if (!client.connected()) {
+			Serial.print("trying to reconnect...");
+			reconnect();
+		}
 
-  // Desired sample rate T=7812microseconds
-  if (micros() - lastMicros > 7812) {
-	 lastMicros = micros();
-	 
-	 // float temperatureC = getTemperature() ;
-	 float ecg = getECG();
-	 sprintf(msg,"%f",ecg);
-	 // Serial.print("temperature read");
-
-	 if (!client.connected()) {
-		Serial.print("trying to reconnect...");
-		reconnect();
-	 }
-
-	 client.publish("mq2_mqtt", msg);
-	 Serial.print("Payload: ");
-	 Serial.println(msg);
-  }
+		client.publish("mq2_mqtt", msg);
+		Serial.print("Payload: ");
+		Serial.println(msg);
+	}
 }
